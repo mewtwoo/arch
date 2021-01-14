52b309e746e8ffdf16588f604c5c41288002341a
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-14 16:36:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
 # file systems utilities
-pacman -S btrfs-progs dosfstools exfatprogs e2fsprogs ntfs-3g
+pacman -S \
+          btrfs-progs \
+          dosfstools \
+          e2fsprogs \
+          exfatprogs \
+          ntfs-3g
+
+# fonts
+pacman -S \
+          adobe-source-han-sans-otc-fonts \
+          adobe-source-han-serif-otc-fonts \
+          noto-fonts \
+          noto-fonts-cjk \
+          noto-fonts-emoji \
+          noto-fonts-extra \
+          ttf-font
+          
+          
