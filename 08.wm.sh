a412cd0b13c4ccb8d1d3952ec32de92536a8b28f
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-15 10:34:02 +0800

    Create 08.wm.sh

---
+++ 08.wm.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo pacman -S yay
+
+# wm
+sudo pacman -S bspwm sxhkd alacritty picom
+sudo yay -S polybar
+
+# lightdm
+sudo pacman -S lightdm lightdm-gtk-greeter
+sudo systemctl enable lightdm
+
+mkdir -p ~/.config/{alacritty,bspwm,picom,polybar,sxhkd}
+
+cp /usr/share/doc/alacritty/example/alacritty.yml ~/.config/alacritty/alacritty.yml
+# cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+cp /etc/xdg/picom.conf.example ~/.config/picom/picom.conf
+cp /usr/share/doc/polybar/config ~/.config//polybar/config
+# cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+$ install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+$ install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
