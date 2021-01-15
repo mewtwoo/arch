5ee14c8fce3cdbd996da98a74c618aedc53e1d57
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-15 11:58:14 +0800

    Update 04.fonts.sh

--- 04.fonts.sh
+++ 04.fonts.sh
@@ -4,8 +4,14 @@
 sudo pacman -S \
           adobe-source-han-sans-otc-fonts \
           adobe-source-han-serif-otc-fonts \
+          gnu-free-fonts \
           noto-fonts \
           noto-fonts-cjk \
           noto-fonts-emoji \
           noto-fonts-extra \
-          ttf-font
+          ttf-bitstream-vera \
+          ttf-croscore \
+          ttf-dejavu \
+          ttf-droid \
+          ttf-ibm-plex \
+          ttf-liberation
