e3432be3dc9e9cbe2842d39360e89be55520cccb
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-15 11:32:06 +0800

    Update 07.file_systems.sh

--- 07.file_systems.sh
+++ 07.file_systems.sh
@@ -2,8 +2,8 @@
 
 # file systems utilities
 sudo pacman -S \
-          btrfs-progs \ # brtfs
-          dosfstools \  # vfat
-          e2fsprogs \   # ext3 ext4
-          exfatprogs \  # exFat
-          ntfs-3g       # ntfs
+          btrfs-progs \
+          dosfstools \
+          e2fsprogs \
+          exfatprogs \
+          ntfs-3g
