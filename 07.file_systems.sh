d3e480aaed5ae089567c5b82e6f8edb8bdd4b08a
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-15 11:32:29 +0800

    Update 07.file_systems.sh

--- 07.file_systems.sh
+++ 07.file_systems.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
 # file systems utilities
-sudo pacman -S \
-          btrfs-progs \
-          dosfstools \
-          e2fsprogs \
-          exfatprogs \
-          ntfs-3g
+sudo pacman -S btrfs-progs dosfstools e2fsprogs exfatprogs ntfs-3g
