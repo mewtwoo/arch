92460a25844b9e5c56850702a268851ac677eac5
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-15 09:50:27 +0800

    Update 03.graphic.sh

--- 03.graphic.sh
+++ 03.graphic.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
 # xorg
-pacman -S xorg-server
+sudo pacman -S xorg-server
 
 # amdgpu
-pacman -S \
+sudo pacman -S \
           mesa lib32-mesa \
           xf86-video-amdgpu \
           vulkan-radeon lib32-vulkan-radeon \
           libva-mesa-driver lib32-libva-mesa-driver \
           mesa-vdpau lib32-mesa-vdpau \
           opencl-mesa lib32-opencl-mesa ocl-icd
+
+# Xorg configuration
+sudo echo '
+Section "Device"
+     Identifier "AMD"
+     Driver "amdgpu"
+     Option "TearFree" "true"
+EndSection
+' >> /etc/X11/xorg.conf.d/20-amdgpu.conf
+
+echo '
+Please add "amdgpu" to "/etc/mkinitcpio.conf"
+like: MODULES=(amdgpu)
+then run "sudo mkinitcpio -p linux"
+'
