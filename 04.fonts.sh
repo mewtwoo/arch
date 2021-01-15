8f7e745728a2db50c8b2a7a7e2a3e81ccf4e4e1f
mewtwoo <76642250+mewtwoo@users.noreply.github.com>
2021-01-15 09:51:30 +0800

    Create 04.fonts.sh

---
+++ 04.fonts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# fonts
+sudo pacman -S \
+          adobe-source-han-sans-otc-fonts \
+          adobe-source-han-serif-otc-fonts \
+          noto-fonts \
+          noto-fonts-cjk \
+          noto-fonts-emoji \
+          noto-fonts-extra \
+          ttf-font
